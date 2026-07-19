8d5b544c9985d105ed91009e217d48a3d5bdf05d
alfashilla-oss <alfashilla@gmail.com>
2026-07-19 13:58:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("Photobooth siap!");
+const video=document.getElementById("video");
+
+const canvas=document.getElementById("canvas");
+
+const startCamera=document.getElementById("startCamera");
+
+const takePhoto=document.getElementById("takePhoto");
